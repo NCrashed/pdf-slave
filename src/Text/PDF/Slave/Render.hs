35b04fe0220131fa2d9c722913231cc905874855
Anton Gushcha <ncrashed@gmail.com>
2016-12-19 18:16:02 +0300

    Split abstraction of Template and TemplateFile

--- src/Text/PDF/Slave/Render.hs
+++ src/Text/PDF/Slave/Render.hs
@@ -1,7 +1,7 @@
 -- | Rendering of templates
 module Text.PDF.Slave.Render(
     PDFContent
-  , renderTemplateFile
+  , renderTemplateToPDF
   -- * Low-level
   , DepFlags
   , DepFlag(..)
@@ -12,13 +12,11 @@ module Text.PDF.Slave.Render(
 
 import Data.ByteString (ByteString)
 import Data.Set (Set)
-import Filesystem.Path.CurrentOS (directory)
 import GHC.Generics
 import Prelude hiding (FilePath)
 import Shelly
 
 import qualified Data.Foldable as F
-import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
 import Text.PDF.Slave.Template
@@ -27,25 +25,27 @@ import Text.PDF.Slave.Template
 type PDFContent = ByteString
 
 -- | Render template and return content of resulted PDF file
-renderTemplateFile :: Template -- ^ Input template
+renderTemplateToPDF :: TemplateFile -- ^ Input template
+  -> FilePath -- ^ Base directory
   -> Sh PDFContent -- ^ Output PDF file
-renderTemplateFile t@Template{..} = withTmpDir $ \outputFolder -> do
-  renderPdfTemplate t outputFolder
-  readBinary (outputFolder </> templateName <.> "pdf")
+renderTemplateToPDF t@TemplateFile{..} baseDir = withTmpDir $ \outputFolder -> do
+  renderPdfTemplate t baseDir outputFolder
+  readBinary (outputFolder </> templateFileName <.> "pdf")
 
 -- | Low-level render of template from .htex to .pdf that is recursively used for dependencies
-renderPdfTemplate :: Template -- ^ Template to render
+renderPdfTemplate :: TemplateFile -- ^ Template to render
+  -> FilePath -- ^ Base directory
   -> FilePath -- ^ Output folder
   -> Sh ()
-renderPdfTemplate t@Template{..} outputFolder = do
-  flags <- renderTemplate t outputFolder
+renderPdfTemplate t@TemplateFile{..} baseDir outputFolder = do
+  flags <- renderTemplate t baseDir outputFolder
   -- define commands of compilation pipe
   let pdflatex = bash "pdflatex" [
           "-synctex=1"
         , "-interaction=nonstopmode"
-        , toTextArg $ outputFolder </> templateName <.> "tex" ]
+        , toTextArg $ outputFolder </> templateFileName <.> "tex" ]
       bibtex = bash "bibtex" [
-          toTextArg $ outputFolder </> templateName <.> "aux" ]
+          toTextArg $ outputFolder </> templateFileName <.> "aux" ]
   -- read flags and construct pipe
   _ <- if S.member NeedBibtex flags
     then pdflatex -|- bibtex -|- pdflatex
@@ -53,14 +53,13 @@ renderPdfTemplate t@Template{..} outputFolder = do
   return ()
 
 -- | Low-level render of template from .htex to .tex that is recursively used for dependencies
-renderTemplate :: Template -- ^ Template to render
+renderTemplate :: TemplateFile -- ^ Template to render
+  -> FilePath -- ^ Base directory
   -> FilePath -- ^ Output folder
   -> Sh DepFlags -- ^ Flags that affects compilation upper in the deptree
-renderTemplate Template{..} outputFolder = do
-  let htexFile = outputFolder </> templateName <.> "htex"
-  writefile htexFile templateBody
-  depFlags <- M.traverseWithKey (renderTemplateDep outputFolder) templateDeps
-  _ <- bash "haskintex" [toTextArg htexFile]
+renderTemplate TemplateFile{..} baseDir outputFolder = do
+  depFlags <- traverse (renderTemplateDep baseDir outputFolder) templateFileDeps
+  _ <- bash "haskintex" [toTextArg $ baseDir </> templateFileBody]
   return $ F.foldMap id depFlags -- merge flags
 
 -- | Collected dependency markers (for instance, that we need bibtex compilation)
@@ -71,22 +70,14 @@ data DepFlag = NeedBibtex -- ^ We need a bibtex compliation
   deriving (Generic, Show, Ord, Eq)
 
 -- | Render template dependency
-renderTemplateDep :: FilePath  -- ^ Output folder
-  -> TemplateName -- ^ Name of dependency (includes file extension)
-  -> TemplateDependency -- ^ Dependency type
+renderTemplateDep :: FilePath -- ^ Base directory
+  -> FilePath  -- ^ Output folder
+  -> TemplateDependencyFile -- ^ Dependency type
   -> Sh DepFlags
-renderTemplateDep outputFolder name dep = case dep of
-  BibtexDep bibContent -> do
-    let filename = outputFolder </> name
-    mkdir_p $ directory filename
-    writefile filename bibContent
-    return $ S.singleton NeedBibtex
-  TemplateDep template -> renderTemplate template outputFolder
-  TemplatePdfDep template -> do
-    renderPdfTemplate template outputFolder
+renderTemplateDep baseDir outputFolder dep = case dep of
+  BibtexDepFile _ -> return $ S.singleton NeedBibtex
+  TemplateDepFile template -> renderTemplate template baseDir outputFolder
+  TemplatePdfDepFile template -> do
+    renderPdfTemplate template baseDir outputFolder
     return mempty
-  OtherDep cnt -> do
-    let filename = outputFolder </> name
-    mkdir_p $ directory filename
-    writeBinary filename cnt
-    return mempty
+  OtherDepFile _ -> return mempty
--- src/Text/PDF/Slave/Template.hs
+++ src/Text/PDF/Slave/Template.hs
@@ -1,4 +1,5 @@
 -- | Defines document template
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Text.PDF.Slave.Template(
     TemplateName
   , TemplateInput
@@ -8,14 +9,26 @@ module Text.PDF.Slave.Template(
   , BibTexBody
   , TemplateDependency(..)
   , Template(..)
+  , TemplateDependencyFile(..)
+  , TemplateFile(..)
+  -- * Helpers
+  , loadTemplateInMemory
+  , storeTemplateInFiles
   ) where
 
-import Data.Text
-import Data.Yaml
-import GHC.Generics
+import Control.Monad (mzero)
 import Data.ByteString (ByteString)
+import Data.Monoid
+import Data.Text as T
+import Data.Yaml
+import Filesystem.Path.CurrentOS (directory)
+import GHC.Generics
+import Prelude hiding (FilePath)
+import Shelly as Sh
 
-import qualified Data.Map.Strict as M
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.Map.Strict        as M
+import qualified Data.Text.Encoding     as T
 
 -- | Template unique name
 type TemplateName = Text
@@ -48,6 +61,39 @@ data TemplateDependency =
   | OtherDep DependencyBody
   deriving (Generic, Show)
 
+instance FromJSON TemplateDependency where
+  parseJSON (Object o) = do
+    depType <- o .: "type"
+    case T.toLower . T.strip $ depType of
+      "bibtex"       -> BibtexDep       <$> o .: "body"
+      "template"     -> TemplateDep     <$> o .: "body"
+      "template_pdf" -> TemplatePdfDep  <$> o .: "body"
+      "other"        -> do
+        t <- o .: "body"
+        either (\e -> fail $ "Cannot decode dependency body (base64): " <> e) (return . OtherDep) $
+          B64.decode . T.encodeUtf8 $ t
+      _ -> fail $ "Unknown template type " <> unpack depType
+  parseJSON _ = mzero
+
+instance ToJSON TemplateDependency where
+  toJSON d = case d of
+    BibtexDep body -> object [
+        "type" .= ("bibtex" :: Text)
+      , "body" .= body
+      ]
+    TemplateDep body -> object [
+        "type" .= ("template" :: Text)
+      , "body" .= body
+      ]
+    TemplatePdfDep body -> object [
+        "type" .= ("template_pdf" :: Text)
+      , "body" .= body
+      ]
+    OtherDep bs -> object [
+        "type" .= ("template_pdf" :: Text)
+      , "body" .= (T.decodeUtf8 . B64.encode $ bs)
+      ]
+
 -- | Description of document template
 data Template = Template {
   -- | Template has human readable name
@@ -59,3 +105,161 @@ data Template = Template {
   -- | Template dependencies (bibtex, listings, other htex files)
   , templateDeps      :: M.Map TemplateName TemplateDependency
   } deriving (Generic, Show)
+
+instance FromJSON Template where
+  parseJSON (Object o) = Template
+    <$> o .: "name"
+    <*> o .: "input"
+    <*> o .: "body"
+    <*> o .: "dependencies"
+  parseJSON _ = mzero
+
+instance ToJSON Template where
+  toJSON Template{..} = object [
+      "name"         .= templateName
+    , "input"        .= templateInput
+    , "body"         .= templateBody
+    , "dependencies" .= templateDeps
+    ]
+
+-- | Same as 'TemplateDependency' but keeps contents in separate files
+data TemplateDependencyFile =
+  -- | Bibtex file for references to other documents. Need call to bibtex
+    BibtexDepFile FilePath
+  -- | HTex file that need to be compiled to .tex file
+  | TemplateDepFile TemplateFile
+  -- | HTex file that need to be compiled to .pdf file
+  | TemplatePdfDepFile TemplateFile
+  -- | Any other file that doesn't need a compilation (listings, images, etc)
+  | OtherDepFile FilePath
+  deriving (Generic, Show)
+
+instance FromJSON FilePath where
+  parseJSON (String s) = return $ Sh.fromText s
+  parseJSON _ = mzero
+
+instance ToJSON FilePath where
+  toJSON = String . Sh.toTextIgnore
+
+instance FromJSON TemplateDependencyFile where
+  parseJSON (Object o) = do
+    depType <- o .: "type"
+    case T.toLower . T.strip $ depType of
+      "bibtex"       -> BibtexDepFile       <$> o .: "body"
+      "template"     -> TemplateDepFile     <$> o .: "body"
+      "template_pdf" -> TemplatePdfDepFile  <$> o .: "body"
+      "other"        -> OtherDepFile        <$> o .: "body"
+      _ -> fail $ "Unknown template type " <> unpack depType
+  parseJSON _ = mzero
+
+instance ToJSON TemplateDependencyFile where
+  toJSON d = case d of
+    BibtexDepFile body -> object [
+        "type" .= ("bibtex" :: Text)
+      , "body" .= body
+      ]
+    TemplateDepFile body -> object [
+        "type" .= ("template" :: Text)
+      , "body" .= body
+      ]
+    TemplatePdfDepFile body -> object [
+        "type" .= ("template_pdf" :: Text)
+      , "body" .= body
+      ]
+    OtherDepFile body -> object [
+        "type" .= ("template_pdf" :: Text)
+      , "body" .= body
+      ]
+
+-- | Same as 'Template', but holds info about template content and dependencies
+-- in other files.
+data TemplateFile = TemplateFile {
+  -- | Template has human readable name
+    templateFileName      :: TemplateName
+  -- | Template expects input in YAML format. The field contains filename of the YAML file.
+  , templateFileInput     :: FilePath
+  -- | Template contents filename.
+  , templateFileBody      :: FilePath
+  -- | Template dependencies (bibtex, listings, other htex files)
+  , templateFileDeps      :: M.Map TemplateName TemplateDependencyFile
+  } deriving (Generic, Show)
+
+instance FromJSON TemplateFile where
+  parseJSON (Object o) = TemplateFile
+    <$> o .: "name"
+    <*> o .: "input"
+    <*> o .: "body"
+    <*> o .: "dependencies"
+  parseJSON _ = mzero
+
+instance ToJSON TemplateFile where
+  toJSON TemplateFile{..} = object [
+      "name"         .= templateFileName
+    , "input"        .= templateFileInput
+    , "body"         .= templateFileBody
+    , "dependencies" .= templateFileDeps
+    ]
+
+-- | Load all external references of template into memory
+loadTemplateInMemory :: TemplateFile -> Sh (Either ParseException Template)
+loadTemplateInMemory TemplateFile{..} = do
+  inputCnt <- readBinary templateFileInput
+  body <- readfile templateFileBody
+  deps <- traverse loadDep templateFileDeps
+  return $ Template
+    <$> pure templateFileName
+    <*> decodeEither' inputCnt
+    <*> pure body
+    <*> sequence deps
+  where
+    loadDep d = case d of
+      BibtexDepFile body -> do
+        cnt <- readfile body
+        return . pure $ BibtexDep cnt
+      TemplateDepFile body -> do
+        tmpl <- loadTemplateInMemory body
+        return $ TemplateDep <$> tmpl
+      TemplatePdfDepFile body -> do
+        tmpl <- loadTemplateInMemory body
+        return $ TemplatePdfDep <$> tmpl
+      OtherDepFile body -> do
+        cnt <- readBinary body
+        return . pure $ OtherDep cnt
+
+-- | Extract all external references of template into file system
+storeTemplateInFiles :: Template -> FilePath -> Sh TemplateFile
+storeTemplateInFiles Template{..} folder = do
+  let inputName = folder </> templateName <> ("_name" :: FilePath) <.> "yaml"
+  writeBinary inputName $ encode templateInput
+  let bodyName = folder </> templateName <.> "htex"
+  mkdir_p $ directory bodyName
+  writefile bodyName templateBody
+  deps <- M.traverseWithKey storeDep templateDeps
+  return $ TemplateFile {
+      templateFileName = templateName
+    , templateFileInput = inputName
+    , templateFileBody = bodyName
+    , templateFileDeps = deps
+    }
+  where
+    storeDep name d = case d of
+      BibtexDep body -> do
+        let bodyName = folder </> name <.> "bib"
+        mkdir_p $ directory bodyName
+        writefile bodyName body
+        return $ BibtexDepFile bodyName
+      TemplateDep template -> do
+        let subfolderName = Sh.fromText name
+        mkdir_p subfolderName
+        dep <- storeTemplateInFiles template subfolderName
+        return $ TemplateDepFile dep
+      TemplatePdfDep template -> do
+        let subfolderName = Sh.fromText name
+        mkdir_p subfolderName
+        dep <- storeTemplateInFiles template subfolderName
+        return $ TemplatePdfDepFile dep
+      OtherDep body -> do
+        let bodyName = folder </> name
+        mkdir_p $ directory bodyName
+        writeBinary bodyName body
+        return $ OtherDepFile bodyName
