dcfb2d0595ce44292d773a396b8905f38ecc26c3
Anton Gushcha <ncrashed@gmail.com>
2016-12-20 13:53:27 +0300

    Add reexports

--- src/Text/PDF/Slave.hs
+++ src/Text/PDF/Slave.hs
@@ -1,5 +1,23 @@
 -- | Reexports
 module Text.PDF.Slave(
-
+  -- * Template definition
+    TemplateName
+  , TemplateInput
+  , TemplateBody
+  , TemplateBibtex
+  , DependencyBody
+  , BibTexBody
+  , TemplateDependency(..)
+  , Template(..)
+  , TemplateDependencyFile(..)
+  , TemplateFile(..)
+  -- ** Bundle helpers
+  , loadTemplateInMemory
+  , storeTemplateInFiles
+  -- * Template rendering to PDF
+  , PDFContent
+  , renderTemplateToPDF
   ) where
 
+import Text.PDF.Slave.Render
+import Text.PDF.Slave.Template
